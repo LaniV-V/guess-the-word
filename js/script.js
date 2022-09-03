a246b98ad2adb357f188b94dbb95cd63ba33774d
lani vv <leonorehall@gmail.com>
2022-09-03 12:20:47 -0400

    adding a xtra bit of code so all works well

--- js/script.js
+++ js/script.js
@@ -117,6 +117,7 @@ const updateGuessesRemaining = function (guess) {
 
   if (remainingGuesses === 0) {
     message.innerHTML = `Game over! The word was <span class="highlight">${word}</span>.`;
+    startOver();
   } else if (remainingGuesses === 1) {
     remainingGuessesSpan.innerText = `${remainingGuesses} guess`;
   } else {
